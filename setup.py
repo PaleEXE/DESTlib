2823cbbc03c7bca13ffc4cce58103807694ec86a
Abed Arrahman Alhennawi <you@example.com>
2024-12-27 16:40:17 +0300

    first commit

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
         version='0.0.1',
         packages=find_packages(),
         install_requires=[
-            'matplotlib~=3.10.0',
-            'numpy~=2.2.1',
-            'pandas~=2.2.3',
+            'matplotlib',
+            'numpy',
+            'pandas',
         ]
 )
